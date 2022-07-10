665fcfbfa0a95a8d6990c6de348d3a2b46376242
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-10 19:00:28 +0200

    Log error

--- back/src/middlewares/error_middleware.ts
+++ back/src/middlewares/error_middleware.ts
@@ -20,6 +20,8 @@ const error: Middleware = async (ctx: Context, next: () => Promise<unknown>) => 
 
         if (ENV === 'dev') {
             console.error(status, ctx.request.url.href, err);
+        } else {
+            await Deno.writeTextFile('error.log', `${status} ${ctx.request.url.href}\n${err}\n`);
         }
 
         ctx.response.status = status;
