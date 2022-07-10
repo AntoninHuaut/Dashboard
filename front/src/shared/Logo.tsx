7c99ad1b6eb92e5e3da8885b6a75d8db996a4b5b
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-10 14:30:16 +0200

    Unused logo

--- front/src/shared/Logo.tsx
+++
@@ -1,8 +0,0 @@
-interface LogoProps {
-    height: number;
-    width: number;
-}
-
-export function Logo(props: LogoProps) {
-    return <img src="/logo.svg" height={props.height} width={props.width} alt="Application logo" />;
-}
