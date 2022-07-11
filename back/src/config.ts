126004e804996c6c4996290d8a5108fb76ab9f50
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-11 21:08:49 +0200

    Create postgres docker & fix env

--- back/src/config.ts
+++ back/src/config.ts
@@ -1,5 +1,10 @@
 import { config as loadEnv } from 'dotenv';
 
-const config = { ...Deno.env.toObject(), ...loadEnv() };
+const config = {
+    ...Deno.env.toObject(),
+    ...loadEnv({
+        path: `.env.${Deno.env.get('ENV')}`,
+    }),
+};
 
 export default config;
