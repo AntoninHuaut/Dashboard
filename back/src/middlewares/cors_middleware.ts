7af3296b1b16089c3900e113c0d62d49b6895461
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-11 13:52:43 +0200

    Access-Control-Allow-Headers

--- back/src/middlewares/cors_middleware.ts
+++ back/src/middlewares/cors_middleware.ts
@@ -10,12 +10,13 @@ if (ENV !== 'dev' && !CORS_ORIGIN) {
 }
 
 const cors: Middleware = async (ctx: Context, next: () => Promise<unknown>) => {
-    await next();
-
     if (ctx.request.url.pathname.startsWith(API_ROUTE)) {
-        ctx.response.headers.set('Access-Control-Allow-Origin', ENV === 'dev' ? '*' : CORS_ORIGIN);
         ctx.response.headers.set('Access-Control-Allow-Credentials', 'true');
+        ctx.response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        ctx.response.headers.set('Access-Control-Allow-Origin', ENV === 'dev' ? '*' : CORS_ORIGIN);
     }
+
+    await next();
 };
 
 export default cors;
