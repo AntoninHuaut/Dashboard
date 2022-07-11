6ceaba75e19c37bb074b0dcb90333f0b26aae1dc
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-11 22:54:44 +0200

    Create default admin at startup

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,6 +1,7 @@
 import { Application } from 'oak';
 import { router } from '/routes/routes.ts';
 import setupMiddlewares from '/middlewares/middlewares.ts';
+import { createDefaultAdmin } from '/db/db.ts';
 
 const PORT = +(Deno.env.get('PORT') ?? 8000);
 const app = new Application();
@@ -8,6 +9,8 @@ const app = new Application();
 setupMiddlewares(app);
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+await createDefaultAdmin();
 
 console.log(`Listening on localhost:${PORT}`);
 await app.listen({ port: PORT });
