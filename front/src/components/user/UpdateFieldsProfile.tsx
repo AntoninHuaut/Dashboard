1c38298fd05ced474e58fc738a39c2349232e46d
Antonin Huaut <antonin.huaut@gmail.com>
2022-07-11 13:23:19 +0200

    ProfilePage: add validation on update fields

--- front/src/components/user/UpdateFieldsProfile.tsx
+++ front/src/components/user/UpdateFieldsProfile.tsx
@@ -1,11 +1,12 @@
 import { ActionIcon, TextInput, useMantineTheme } from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
-import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
+import { ChangeEvent, useCallback, useEffect, useLayoutEffect, useState } from 'react';
 import { Check, Edit, Mail, User, X } from 'tabler-icons-react';
 
 import { useFetch } from '../../api/request';
 import { updateRequest } from '../../api/user_request';
 import { useAuth } from '../../hooks/useAuth';
+import { EmailInput, isValidEmail } from '../form/EmailInput';
 
 type FieldNameType = 'email' | 'username';
 
@@ -18,6 +19,7 @@ export function UpdateFieldProfile() {
     const [username, setUsername] = useState(auth.user.username);
 
     const [isLoading, setLoading] = useState(false);
+    const [isValidInput, setValidInput] = useState(true);
     const [editFieldName, setEditFieldName] = useState<string>('');
 
     const editButton = useCallback(
@@ -49,10 +51,10 @@ export function UpdateFieldProfile() {
 
                     {editFieldName === fieldName && !isLoading && (
                         <>
-                            <ActionIcon color={'lime'} variant="filled" size="md" mr="2px" onClick={validateChange} disabled={isLoading}>
+                            <ActionIcon color={'lime'} variant="filled" size="md" mr="2px" onClick={validateChange} disabled={isLoading || !isValidInput}>
                                 <Check />
                             </ActionIcon>
-                            <ActionIcon color={'red'} variant="filled" size="md" onClick={cancelChange} disabled={isLoading}>
+                            <ActionIcon color={'red'} variant="filled" size="md" onClick={cancelChange} disabled={isLoading || !isValidInput}>
                                 <X />
                             </ActionIcon>
                         </>
@@ -73,21 +75,34 @@ export function UpdateFieldProfile() {
 
     const createTextInput = useCallback(
         (icon: JSX.Element, fieldName: FieldNameType, fieldValue: string, setFieldValue: (value: string) => any) => {
-            return (
-                <TextInput
-                    icon={icon}
-                    placeholder={`Your ${fieldName}`}
-                    value={fieldValue}
-                    rightSection={editButton(fieldName, fieldValue, setFieldValue)}
-                    rightSectionWidth={editFieldName === fieldName && !isLoading ? 67 : 40}
-                    disabled={(editFieldName !== fieldName && !isLoading) || isLoading}
-                    onChange={(event) => setFieldValue(event.currentTarget.value)}
-                    mt="sm"
-                />
-            );
+            const config = {
+                icon,
+                placeholder: `Your ${fieldName}`,
+                value: fieldValue,
+                rightSection: editButton(fieldName, fieldValue, setFieldValue),
+                rightSectionWidth: editFieldName === fieldName && !isLoading ? 67 : 40,
+                disabled: (editFieldName !== fieldName && !isLoading) || isLoading,
+                onChange: (event: ChangeEvent<HTMLInputElement>) => setFieldValue(event.currentTarget.value),
+                mt: 'sm',
+                required: true,
+            };
+
+            if (fieldName === 'email') {
+                return <EmailInput {...config} />;
+            } else {
+                return <TextInput {...config} label="Username" />;
+            }
         },
         [editButton, updateFetch]
     );
+
+    useEffect(() => {
+        if (editFieldName === 'email') {
+            setValidInput(isValidEmail(email));
+        } else if (editFieldName === 'username') {
+            setValidInput(username.length > 0);
+        }
+    }, [email, username]);
 
     useLayoutEffect(() => {
         if (updateFetch.cannotHandleResult()) return;
