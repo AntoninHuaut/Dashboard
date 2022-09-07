abb0380ad1c1cbb7644f7765299e0e234ef5962e
Antonin Huaut <antonin.huaut@gmail.com>
2022-09-07 20:10:40 +0200

    Fix CORS header (Authorization)

--- back/src/middlewares/cors_middleware.ts
+++ back/src/middlewares/cors_middleware.ts
@@ -6,7 +6,7 @@ import { API_ROUTE } from '/routes/routes.ts';
 const cors: Middleware = async (ctx: Context, next: () => Promise<unknown>) => {
     if (ctx.request.url.pathname.startsWith(API_ROUTE)) {
         ctx.response.headers.set('Access-Control-Allow-Credentials', 'true');
-        ctx.response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        ctx.response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         ctx.response.headers.set('Access-Control-Allow-Methods', 'DELETE, GET, OPTIONS, POST, PUT');
         ctx.response.headers.set('Access-Control-Allow-Origin', config.BASE_FRONT_URL);
     }
