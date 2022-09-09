c625a3b422b561e6818e84d03763c4ecce5ffcee
Antonin Huaut <antonin.huaut@gmail.com>
2022-09-09 18:38:42 +0200

    Update API CORS

--- back/src/middlewares/cors_middleware.ts
+++ back/src/middlewares/cors_middleware.ts
@@ -1,14 +1,19 @@
 import { Context, Middleware } from 'oak';
 
 import { config } from '/config.ts';
-import { API_ROUTE } from '/routes/routes.ts';
+import { API_ROUTE, API_ROUTE_APP_TRACKMAIL } from '/routes/routes.ts';
 
 const cors: Middleware = async (ctx: Context, next: () => Promise<unknown>) => {
     if (ctx.request.url.pathname.startsWith(API_ROUTE)) {
         ctx.response.headers.set('Access-Control-Allow-Credentials', 'true');
         ctx.response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         ctx.response.headers.set('Access-Control-Allow-Methods', 'DELETE, GET, OPTIONS, POST, PUT');
-        ctx.response.headers.set('Access-Control-Allow-Origin', config.BASE_FRONT_URL);
+
+        if (ctx.request.url.pathname.startsWith(API_ROUTE_APP_TRACKMAIL)) {
+            ctx.response.headers.set('Access-Control-Allow-Origin', '*');
+        } else {
+            ctx.response.headers.set('Access-Control-Allow-Origin', config.BASE_FRONT_URL);
+        }
     }
 
     await next();
--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -10,8 +10,8 @@ export enum RouteEnum {
     APP_TRACKMAIL = 'app/trackmail',
 }
 
-export const API_ROUTE = '/api';
-const API_ROUTE_v1 = `${API_ROUTE}/v1`;
+export const API_ROUTE = '/api/v1';
+export const API_ROUTE_APP_TRACKMAIL = `${API_ROUTE}/${RouteEnum.APP_TRACKMAIL}`;
 
 export const router = new Router();
 const api = new Router();
@@ -24,6 +24,6 @@ api.use(`/${RouteEnum.AUTH}`, authRouter.routes());
 api.use(`/${RouteEnum.USER}`, userRouter.routes());
 api.use(`/${RouteEnum.APP_TRACKMAIL}`, trackMailRouter.routes());
 
-router.use(`${API_ROUTE_v1}`, api.routes());
+router.use(`${API_ROUTE}`, api.routes());
 
-export const getAuthRoute = () => `${API_ROUTE_v1}/${RouteEnum.AUTH}`;
+export const getAuthRoute = () => `${API_ROUTE}/${RouteEnum.AUTH}`;
